7374750b5151e5b0c231ec6ad054d05e4d7a4e1c
Sebastien Aucante <sebastien.aucante@gmail.com>
2025-08-15 20:55:52 +0200

    Feat: Refund List UI

--- src/app/demo/components/refund/refund-list/refund-list.component.ts
+++ src/app/demo/components/refund/refund-list/refund-list.component.ts
@@ -1,11 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+export interface RefundResponseDTO {
+    id: number;
+    loanId: number;
+    amount: number;
+    refundDate: string;
+    status: string;
+}
 
 @Component({
-  selector: 'app-refund-list',
-  standalone: false,
-  templateUrl: './refund-list.component.html',
-  styleUrl: './refund-list.component.scss'
+    selector: 'app-refund-list',
+    templateUrl: './refund-list.component.html',
+    styleUrls: ['./refund-list.component.scss']
 })
-export class RefundListComponent {
+export class RefundListComponent implements OnInit {
+    refunds: RefundResponseDTO[] = [];
+    first: number = 0;
+    rows: number = 5;
 
+    constructor(private http: HttpClient) {}
+
+    ngOnInit() {
+        this.http.get<RefundResponseDTO[]>('/api/refunds').subscribe({
+            next: (data) => {
+                console.log('Données mockées /api/refunds :', data);
+                this.refunds = data;
+            },
+            error: (err) => {
+                console.error('Erreur lors du fetch /api/refunds', err);
+            }
+        });
+    }
+
+    next() {
+        this.first = this.first + this.rows;
+    }
+
+    prev() {
+        this.first = this.first - this.rows;
+    }
+
+    reset() {
+        this.first = 0;
+    }
+
+    isLastPage(): boolean {
+        return this.refunds ? this.first >= (this.refunds.length - this.rows) : true;
+    }
+
+    isFirstPage(): boolean {
+        return this.first === 0;
+    }
+
+    pageChange(event: any) {
+        this.first = event.first;
+        this.rows = event.rows;
+    }
 }
--- src/app/demo/modules/refund/refund.module.ts
+++ src/app/demo/modules/refund/refund.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RefundRoutingModule } from './refund-routing.module';
+import { RefundListComponent } from "../../components/refund/refund-list/refund-list.component";
+import { ButtonModule } from "primeng/button";
+import { TableModule } from "primeng/table";
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    RefundRoutingModule
-  ]
+  declarations: [
+      RefundListComponent
+  ],
+    imports: [
+        CommonModule,
+        RefundRoutingModule,
+        ButtonModule,
+        TableModule
+    ]
 })
 export class RefundModule { }

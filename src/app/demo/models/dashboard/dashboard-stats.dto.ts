ebac7fdc092203dd653be3f0db14f1954757bd66
Sebastien Aucante <sebastien.aucante@gmail.com>
2025-08-16 00:23:24 +0200

    Feat: Dashboards Stats

--- src/app/demo/models/dashboard/dashboard-stats.dto.ts
+++ src/app/demo/models/dashboard/dashboard-stats.dto.ts
@@ -1,3 +1,16 @@
-export interface DashboardStats {
-  stats: number;
+export interface DashboardStatsDTO {
+    totalFinanced: number;
+    totalRevenue: number;
+    totalCustomers: number;
+    newCustomers: number;
+    unreadComments: number;
+    respondedComments: number;
+    loansInProgress: number;
+    loansRepaid: number;
+    refundsInProgress: number;
+    lastUpdate: string;
+    monthlyRefunds: {
+        month: string;
+        totalAmount: number;
+    }[];
 }

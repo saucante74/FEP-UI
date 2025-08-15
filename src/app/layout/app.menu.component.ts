ce2db3cc572e04f14e17387f2f784eec2b551eba
Sebastien Aucante <sebastien.aucante@gmail.com>
2025-08-15 21:26:48 +0200

    Feat: Menu

--- src/app/layout/app.menu.component.ts
+++ src/app/layout/app.menu.component.ts
@@ -18,13 +18,33 @@ export class AppMenuComponent implements OnInit {
                 items: [
                     { label: 'Homepage', icon: 'pi pi-fw pi-home', routerLink: ['/homepage'] },
                     { label: 'Dashboard', icon: 'pi pi-fw pi-home', routerLink: ['/'] },
-                    { label: 'Loan List', icon: 'pi pi-fw pi-home', routerLink: ['/loan/list'] },
-                    { label: 'Loan New', icon: 'pi pi-fw pi-home', routerLink: ['/loan/request'] },
-                    { label: 'Refund List', icon: 'pi pi-fw pi-home', routerLink: ['/refund/list'] },
-                    { label: 'Refund Form', icon: 'pi pi-fw pi-home', routerLink: ['/refund/request'] },
-                    { label: 'Report List', icon: 'pi pi-fw pi-home', routerLink: ['/report/list'] },
-                    { label: 'Report Form', icon: 'pi pi-fw pi-home', routerLink: ['/report/form'] },
-                    { label: 'TDB', icon: 'pi pi-fw pi-home', routerLink: ['/tdb/view'] },
+                ]
+            },
+            {
+                label: 'TABLEAU DE BORD',
+                items: [
+                    { label: 'Tableau de bord', icon: 'pi pi-th-large', routerLink: ['/tdb/view'] },
+                ]
+            },
+            {
+                label: 'PRETS',
+                items: [
+                    { label: 'Liste des prêts', icon: 'pi pi-bars', routerLink: ['/loan/list'] },
+                    { label: 'Ajouter un prêt', icon: 'pi pi-plus', routerLink: ['/loan/request'] },
+                ]
+            },
+            {
+                label: 'Remboursements',
+                items: [
+                    { label: 'Liste des remboursements', icon: 'pi pi-bars', routerLink: ['/refund/list'] },
+                    { label: 'Ajouter un remboursement', icon: 'pi pi-plus', routerLink: ['/refund/request'] },
+                ]
+            },
+            {
+                label: 'Signalements',
+                items: [
+                    { label: 'Liste des signalements', icon: 'pi pi-bars', routerLink: ['/report/list'] },
+                    { label: 'Ajouter un signalement', icon: 'pi pi-plus', routerLink: ['/report/form'] },
                 ]
             },
             {

5329d10473cec39271b342bf3d540ebd1ade1df2
Sebastien Aucante <sebastien.aucante@gmail.com>
2025-08-15 19:46:32 +0200

    Feat: Mocks

--- src/app/demo/modules/loan/loan.module.ts
+++ src/app/demo/modules/loan/loan.module.ts
@@ -2,15 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoanRoutingModule } from './loan-routing.module';
 import { LoanListComponent } from "../../components/loan/loan-list/loan-list.component";
+import { TableModule } from "primeng/table";
+import { ButtonModule } from "primeng/button";
 
 
 @NgModule({
   declarations: [
       LoanListComponent
   ],
-  imports: [
-    CommonModule,
-    LoanRoutingModule
-  ]
+    imports: [
+        CommonModule,
+        LoanRoutingModule,
+        TableModule,
+        ButtonModule
+    ]
 })
 export class LoanModule { }
